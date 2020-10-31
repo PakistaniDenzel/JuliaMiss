b921423e5c14976c1c5b0328f75d1782053bc0b4
PakistaniDenzel <73758461+PakistaniDenzel@users.noreply.github.com>
2020-10-31 21:03:49 +0100

    Update z6.jl

--- Racunarske/z6.jl
+++ Racunarske/z6.jl
@@ -1,3 +1,13 @@
+#=  Neka je matricom A predstavljena prodaja odgovarajućeg proizvoda u
+toku jednog meseca, gde svaka kolona odgovara jednom danu u mesecu a vrsta odgovara
+jednom određenom tipu proizvoda. Element matrice predstavlja broj prodatih proizvoda u
+toku jednog dana, npr. A(i,j)=3 se odnosi na količinu (3) prodatih i-tog proizvoda j-tog dana
+u mesecu. 
+Odrediti:
+    • Najbolje prodavani proizvod u toku meseca,
+    • Dan kada je prodat najveći broj proizvoda,
+    • Kog dana je bilo najviše proizvoda koji se uopšte nisu prodavali. =#
+
 using LinearAlgebra
 
 A = round.(rand(rand(5:30), 30)*100);
